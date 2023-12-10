d99fde11314de5de204b33ae71b0c5913b956121
JoshRand <JoshR.AndersonEng@gmail.com>
2023-12-09 22:24:26 -0600

    updated premake

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,4 @@
-project "imgui"
+project "ImGui"
 	kind "StaticLib"
 	language "C++"
 
@@ -9,14 +9,16 @@ project "imgui"
 	{
 		"imconfig.h",
 		"imgui.h",
-		"imgui.cpp",
-		"imgui_draw.cpp",
 		"imgui_internal.h",
-		"imgui_widgets.cpp",
-		"imgui.h",
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
+		"imgui.cpp",
+		"imgui_demo.cpp",
+		"imgui_draw.cpp",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"imgui.h",
 		"imgui_demo.h",
 	}
 
